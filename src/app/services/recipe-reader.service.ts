c60c7657eb9ea239865649529be7791233888552
rbrock44 <rbrock444@gmail.com>
2023-03-22 16:19:03 -0500

    add up to 140

--- src/app/services/recipe-reader.service.ts
+++ src/app/services/recipe-reader.service.ts
@@ -7,7 +7,7 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeReaderService {
-  recipeTotal = 102;
+  recipeTotal = 140;
   FAVORITE_NAME = "family-recipe-favorites"
 
   constructor(private http: HttpClient) {
