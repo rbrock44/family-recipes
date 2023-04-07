f37fe295f738ccc90e2ac178deb395cab20bbbd2
rbrock44 <rbrock444@gmail.com>
2023-04-07 13:12:58 -0500

    update recipes to 790

--- src/app/services/recipe-reader.service.ts
+++ src/app/services/recipe-reader.service.ts
@@ -7,7 +7,7 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeReaderService {
-  recipeTotal = 750;
+  recipeTotal = 790;
   FAVORITE_NAME = "family-recipe-favorites"
 
   constructor(private http: HttpClient) {
