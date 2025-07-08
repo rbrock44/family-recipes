59940c31dba2d4b0d157277925107950445675b0
rbrock44 <rbrock444@gmail.com>
2025-07-07 20:25:27 -0500

    variable fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -47,7 +47,7 @@ export class HeaderComponent {
 
   homeClick(): void {
     this.service.setEmptyRecipe();
-    this.location.replaceState(this.location.pathname);
+    this.location.replaceState(location.pathname);
   }
 
   forwardClick(): void {
