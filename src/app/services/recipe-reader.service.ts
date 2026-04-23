d69df9b2096dbbfe50ca267c37880a51f0ed2716
R Brock <rbrock444@gmail.com>
2026-04-23 11:00:11 -0500

    Increment recipeTotal from 923 to 924

--- src/app/services/recipe-reader.service.ts
+++ src/app/services/recipe-reader.service.ts
@@ -7,7 +7,7 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeReaderService {
-  recipeTotal = 923;
+  recipeTotal = 924;
   fullyLoaded = false;
   FAVORITE_NAME = "family-recipe-favorites"
 
