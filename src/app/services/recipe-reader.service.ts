71fff9bb5cf676cabbe4d048c31dd5f9a3e2a557
rbrock44 <rbrock444@gmail.com>
2026-08-01 21:06:45 -0500

    add anne brock recipes up to 943

--- src/app/services/recipe-reader.service.ts
+++ src/app/services/recipe-reader.service.ts
@@ -7,7 +7,7 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeReaderService {
-  recipeTotal = 924;
+  recipeTotal = 943;
   fullyLoaded = false;
   FAVORITE_NAME = "family-recipe-favorites"
 
